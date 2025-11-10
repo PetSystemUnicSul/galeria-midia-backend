4ef65e9cb1e2a377c5668f66da0e1350ac4d0491
PetSystemUnicSul <petsystemunicsul@outlook.com>
2025-11-09 22:43:04 -0300

    Update generators.ts (mudança nos códigos gerados)

--- src/utils/generators.ts
+++ src/utils/generators.ts
@@ -4,16 +4,15 @@ import { customAlphabet } from 'nanoid';
 
 /**
  * Gera um código de compartilhamento (share_code) único.
- * * Usamos 'nanoid' para criar um código legível,
- * alfanumérico e com 8 caracteres.
- * * Ex: "4k8sNqP2"
+ * * Que usa apenas números (0-9) e tem 6 dígitos.
+ * * Ex: "458901"
  */
 export const generateShareCode = () => {
-  // Define o alfabeto que queremos usar (números e letras maiúsculas/minúsculas)
-  const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  // definindo numeros que poderam ser usados na sequancis do codigo
+  const alphabet = '0123456789';
   
-  // Configura o nanoid para usar esse alfabeto e ter 8 caracteres
-  const nanoid = customAlphabet(alphabet, 8);
+  // Configura o nanoid para usar esse numeros e ter 6 caracteres
+  const nanoid = customAlphabet(alphabet, 6);
   
   return nanoid();
 };
