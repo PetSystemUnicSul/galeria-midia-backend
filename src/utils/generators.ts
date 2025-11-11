90df5ca4c4e04dc4a4abf67dd125718e794ed477
Lucas Fernandes Ribeiro de Oliveira <136923413+lucas-fro@users.noreply.github.com>
2025-11-11 09:27:21 -0300

    transformando de 6 para 5 digitos

--- src/utils/generators.ts
+++ src/utils/generators.ts
@@ -4,15 +4,15 @@ import { customAlphabet } from 'nanoid';
 
 /**
  * Gera um código de compartilhamento (share_code) único.
- * * Que usa apenas números (0-9) e tem 6 dígitos.
- * * Ex: "458901"
+ * * Que usa apenas números (0-9) e tem 5 dígitos.
+ * * Ex: "45890"
  */
 export const generateShareCode = () => {
   // definindo numeros que poderam ser usados na sequancis do codigo
   const alphabet = '0123456789';
   
-  // Configura o nanoid para usar esse numeros e ter 6 caracteres
-  const nanoid = customAlphabet(alphabet, 6);
+  // Configura o nanoid para usar esse numeros e ter 5 caracteres
+  const nanoid = customAlphabet(alphabet, 5);
   
   return nanoid();
 };
